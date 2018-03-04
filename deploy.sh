e1b988870f6b30140b16c5e55742cd7258e3d95b
Siddharth Gupta <siddharth.gupta@flipkart.com>
2018-03-05 01:25:47 +0530

    Checking Versioning

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,4 @@ sed -i '' 's/BUNDLE_IDENTIFIER/'$2'/g' public/manifest.plist
 sed -i '' 's#IPA_MANIFEST_URL#'$3'#g' public/manifest.plist #Using Delimiter # here because the substitution URL constains slashes
 echo "Configuration Complete, Deploying to now.sh"
 now --public
-echo "Deployment complete to now at"
-echo pbpaste
+echo "Deployment complete to now at" $(pbpaste)
