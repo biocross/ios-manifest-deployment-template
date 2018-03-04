da5db60b36afab70982fdbb2458c21d0c3c68119
Siddharth Gupta <siddharth.gupta@flipkart.com>
2018-03-05 01:11:04 +0530

    Added deploy script

--- configure.sh
+++
@@ -1,8 +0,0 @@
-echo "Configuring Manifest"
-sed -i 's/APP_NAME/{$1}/g' ./public/manifest.plist
-sed -i 's/BUNDLE_IDENTIFIER/{$2}/g' ./public/manifest.plist
-sed -i 's/IPA_MANIFEST_URL/{$3}/g' ./public/manifest.plist
-echo "Configuration Complete, Deploying to now.sh"
-now --public
-echo "Deployment complete to now"
-echo pbpaste
